cf6bb7d8e8a8c8cbc06afe95eefa916d6cfc2374
Varun Rathi <varunrathi28@gmail.com>
2017-11-15 12:18:11 +0530

    Table View functions for populating list

--- MVVM 2/ViewController.swift
+++ MVVM 2/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewModel:ViewModel!
+    @IBOutlet weak var tableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        viewModel.getList {
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,26 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel.appTitleToDisplay(for: indexPath)
+        cell.detailTextLabel?.text = viewModel.appRatingToDisplay(for: indexPath)
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return  1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsToDisplay(in: section)
+    }
+    
+    
 }
 
